5b69c15d605f294e2d70642d8e05cf93286dc338
onofre <brunoonofre64@gmail.com>
2022-03-29 22:54:52 -0300

    feito primeira conexao do eclipse com mysql

---
+++ CursoJava2021/src/jbdc/TesteConexao.java
@@ -0,0 +1,24 @@
+package jbdc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
+		final String usuario = "root";
+		final String senha = "23onofre.,";
+		
+		Connection conexao = DriverManager.getConnection(url, usuario, senha);
+		
+		
+		
+		System.out.println("Conexão concluida com sucesso!");
+		conexao.close();
+		
+	}
+
+}
