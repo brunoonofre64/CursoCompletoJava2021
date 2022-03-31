4b2982c44e68cf2009f25ea82fd5ae6bb5d6ef38
onofre <brunoonofre64@gmail.com>
2022-03-31 00:01:11 -0300

    criada a classe capaz de atualizar a pessoa do banco

---
+++ CursoJava2021/src/jbdc/AtualizarPessoa.java
@@ -0,0 +1,57 @@
+package jbdc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AtualizarPessoa {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe o codigo da pessoa: ");
+		int codigo = scan.nextInt();
+		
+		String select = "SELECT codigo, nome FROM pessoas WHERE codigo = ?";
+		String update = "UPDATE pessoas SET nome = ? WHERE codigo = ?";
+		
+		
+		
+		Connection conexao = FabricaConexao.getConexao();
+		PreparedStatement stmt = conexao.prepareStatement(select);
+		stmt.setInt(1, codigo);
+		ResultSet resultado = stmt.executeQuery();
+		
+		if(resultado.next()) {
+			Pessoa p = new Pessoa(resultado.getInt(1), resultado.getString(2));
+			
+			System.out.println("O nome atual é: " + p.getNome());
+			scan.nextLine();
+			
+			System.out.println("Informe um novo nome: ");
+			String novoNome = scan.nextLine();
+			
+		
+			stmt.close();
+			stmt = conexao.prepareStatement(update);
+			stmt.setString(1, novoNome);
+			stmt.setInt(2, codigo);
+			stmt.execute();
+			stmt.close();
+			
+			System.out.println("Pessoa alterada com sucesso! ");
+		 } else {
+			 System.out.println("Pessoa não encontrada... ");
+		 }
+		
+		
+		
+		
+		scan.close();
+		conexao.close();
+	}
+
+}
